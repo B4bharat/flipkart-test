9617173cc0a53759e09f3ba49355f4b107ae79ce
Bharat Poptwani <bharat.poptwani@bookmyshow.com>
2020-04-22 20:04:42 +0530

    managed to display the data the table

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Add your scripts here
-(function () {
+window.onload = function () {
   // your page initialization code here
   // the DOM will be available here
   /**
@@ -7,8 +7,8 @@
    * X- Create a main container div in HTML
    * X- search bar will always be on top, with 'show selected'
    * - Find how to insert an element into the DOM
-   *  - create a function which attaches a node to the DOM based on the value
-   *  - it should always be of the format <div><select></select><div class="key"></div><div class="value"></div><div class="description"></div></div>
+   *  X- create a function which attaches a node to the DOM based on the value
+   *  X- it should always be of the format <div><select></select><div class="key"></div><div class="value"></div><div class="description"></div></div>
    * - On entering search string, show only those params whose label matches the search string (or a subset of it)
    *    - create a function to handle this on enter event (better if its on keystroke, later enhancement)
    *    - find a way to remove DOM nodes
@@ -17,18 +17,42 @@
    * 
    * - style it in the end.
    */
-  let productConfig;
+  function createTableContent(productConfig) {
+
+    for (let i = 0; i < productConfig.config.length; i++) {
+
+      let configRowBeginHTML = `<div class=\"config-table-row\" data-config="${productConfig.config[i].key}" >`;
+      let checkboxHTML = "<input type=\"checkbox\" name=\"selected\">";
+      let keyHTML = `<div class="key">${productConfig.config[i].label}</div>`;
+      let valueHTML = `<div class="value">${productConfig.config[i].field.defaultValue}</div>`;
+      let descriptionHTML = `<div class="description">${productConfig.config[i].description}</div>`;
+      let configRowEnd = `</div>`;
+
+      let configTableHeader;
+      if (i === 0) {
+        configTableHeader = document.querySelector('[data-config="config-table"]');
+      } else {
+        configTableHeader = document.querySelector(`[data-config="${productConfig.config[i - 1].key}"]`);
+      }
+
+      let configTableRowString = configRowBeginHTML + checkboxHTML + keyHTML + valueHTML + descriptionHTML + configRowEnd;
+      configTableHeader.insertAdjacentHTML("afterend", configTableRowString);
+    }
+  }
+
+  function createConfigTable(config) {
+    createTableContent(config)
+    // it should always be of the format <div><select></select><div class="key"></div><div class="value"></div><div class="description"></div></div>
+  }
 
   async function getProductDetails() {
     let response = await fetch('https://flipkart-configuration-table.now.sh/api');
-    productConfig = await response.json();
-    yoyoma(productConfig);
+    let productConfig = await response.json();
+
     return productConfig;
   }
 
-  getProductDetails();
-
-  function yoyoma(config) {
-    console.log('config', config)
-  }
-})();
+  getProductDetails().then((productConfig) => {
+    createConfigTable(productConfig);
+  });
+};
